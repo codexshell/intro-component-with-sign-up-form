cc0d1e614ce60519b2e29965a6a8125fd8dd65e6
silvanos <ericoderito@gmail.com>
2022-08-21 12:54:57 +0300

    feat(tailwind): extend tailwind
    extend tailwind by adding custom css classes
    the values for the custom classes are extracted from the style-guide.md
    file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'p-red': 'hsl(0, 100%, 74%)',
+				'p-green': 'hsl(154, 59%, 51%)',
+				'a-blue': 'hsl(248, 32%, 49%)',
+				'n-dark-blue': 'hsl(249, 10%, 26%)',
+				'n-grayish-blue': 'hsl(246, 25%, 77%)'
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif']
+			}
+		}
 	},
 	plugins: []
 };
