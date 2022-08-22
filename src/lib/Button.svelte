7bc78fcb76297e7da00a67156d2e74ed8b15e0ee
silvanos <ericoderito@gmail.com>
2022-08-22 12:31:38 +0300

    (button): style button component

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,1 +1,26 @@
 <button type="submit">Claim your free trial</button>
+
+<style>
+	button {
+    position: relative;
+		--flow-space: 1rem;
+		background-color: theme('colors.p-green');
+		color: white;
+		width: 85%;
+		padding-block: theme('padding[3.5]');
+    text-transform: uppercase;
+    font-weight: 500;
+    border-radius: theme("borderRadius.md");
+    letter-spacing: theme("letterSpacing.wide");
+    box-shadow: 0 .25rem 0 0 hsl(154, 59%, 41%);
+	}
+
+  button:hover {
+    background-color: hsl(154, 65%, 65%);
+  }
+
+  button:active {
+    box-shadow: none;
+    bottom: -.25rem;
+  }
+</style>
